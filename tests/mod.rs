b20a679c0c82c85e97e92ba62b281da7f3221aac
jason.zhu <jason.zhu@shopee.com>
2024-05-20 15:56:54 +0800

    fix: support error case for packagejson bad main case

--- tests/mod.rs
+++ tests/mod.rs
@@ -26,56 +26,6 @@ mod tests {
     }
 
     #[test]
-    fn test_resolver_inner_package_json() {
-        let options = ResolveOptions {
-            alias_fields: vec![vec!["browser".into()]],
-            ..ResolveOptions::default()
-        };
-        let fold_path = path_resolve("esbuild-test/src");
-        let dir = Path::new(fold_path.as_str());
-        let resolver = Resolver::new(options.clone());
-        let res = resolver.resolve(dir, "pkg/sub").unwrap();
-        println!("res-> \n {:#?}", res);
-        // up case is right
-        // down case is wrong
-        let fold_path = path_resolve("esbuild-test/src/node_modules/pkg/sub");
-        let dir = Path::new(fold_path.as_str());
-        let resolver = Resolver::new(options);
-        let res = resolver.resolve(dir, "sub").unwrap();
-        // line 30 has error but expected require('sub') to be resolved to './sub/sub' sub/bar.js
-        // because of the browser field in the package.json  "./sub/sub": "./sub/bar.js"
-        println!("res-> \n {:#?}", res);
-    }
-
-    #[test]
-    fn test_resolver_exports_package_json() {
-        let options = ResolveOptions {
-            alias_fields: vec![vec!["browser".into()]],
-            exports_fields: vec![vec!["exports".into()]],
-            condition_names: vec!["node".into(), "import".into(), "require".into()],
-            ..ResolveOptions::default()
-        };
-        let dir = Path::new("/Users/jason.zhu/Desktop/github/rolldown/crates/rolldown/tests/esbuild/packagejson/test_package_json_exports_default_over_import_and_require/src");
-        let resolver = Resolver::new(options);
-        let res = resolver.resolve(dir, "pkg").unwrap();
-        println!("res-> \n {:#?}", res);
-    }
-
-    #[test]
-    fn test_resolver_exports_order_package_json() {
-        let options = ResolveOptions {
-            alias_fields: vec![vec!["browser".into()]],
-            exports_fields: vec![vec!["exports".into()]],
-            condition_names: vec!["node".into(), "import".into(), "require".into()],
-            ..ResolveOptions::default()
-        };
-        let dir = Path::new("/Users/jason.zhu/Desktop/github/rolldown/crates/rolldown/tests/esbuild/packagejson/test_package_json_exports_alternatives/src");
-        let resolver = Resolver::new(options);
-        let res = resolver.resolve(dir, "pkg/apples/green.js").unwrap();
-        println!("res-> \n {:#?}", res);
-    }
-
-    #[test]
     fn test_resolver_package_json_bad_main() {
         let options = ResolveOptions {
             alias_fields: vec![vec!["browser".into()]],
@@ -84,9 +34,17 @@ mod tests {
             main_fields: vec!["main".into()],
             ..ResolveOptions::default()
         };
-        let dir = Path::new("/Users/jason.zhu/Desktop/github/rolldown/crates/rolldown/tests/esbuild/packagejson/test_package_json_bad_main/src");
+        let dir = path_resolve("test_package_json_bad_main/src");
         let resolver = Resolver::new(options);
         let res = resolver.resolve(dir, "demo-pkg").unwrap();
         println!("res-> \n {:#?}", res);
+        /*
+        node_modules/demo-pkg/package.json
+        {
+          "main": "./does-not-exist.js"
+        }
+        the main field in package.json is invalid
+        but oxc_resolver will try to resolve index.js
+         */
     }
 }
